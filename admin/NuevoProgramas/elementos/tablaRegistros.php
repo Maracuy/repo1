58db44ded24d29a73475838fc6c82ee24c62ee63
JoseEduardo3107 <josseduardo7u7@gmail.com>
2021-10-19 13:19:42 -0500

    tabla registros - botones paginacion

--- admin/NuevoProgramas/elementos/tablaRegistros.php
+++ admin/NuevoProgramas/elementos/tablaRegistros.php
@@ -1,4 +1,117 @@
 <!-- Seccion de tabla con contenido [Hacer tabla y agregar paginacion]-->
+
+<!-- Tabla -->
 <div class="ContenedorTablaGeneral">
 
+    <!-- Corresponde a una fila de la tabla [Este es el titulo de las columnas]-->
+    <div class="ContenedorFilasContenido">
+        <div class="ItemFilaC">
+            ID
+        </div>
+        <div class="ItemFilaC">
+            Propiedades
+        </div>
+        <div class="ItemFilaC">
+            Beneficiario
+        </div>
+        <div class="ItemFilaC">
+            Solicitud
+        </div>
+        <div class="ItemFilaC">
+            Departamento
+        </div>
+        <div class="ItemFilaC">
+            Origen
+        </div>
+        <div class="ItemFilaC">
+            Avance
+        </div>
+        <div class="ItemFilaC">
+            Fecha/Hora
+        </div>
+    </div>
+
+    <!-- Corresponde a una fila de la tabla -->
+    <div class="ContenedorFilasContenido">
+        <div class="ItemFilaC">
+            551d8b
+        </div>
+        <div class="ItemFilaC">
+            {iconos}
+        </div>
+        <div class="ItemFilaC">
+            Nombre (CURP)
+        </div>
+        <div class="ItemFilaC">
+            Solicitud-Ejemplo
+        </div>
+        <div class="ItemFilaC">
+            Departamento Inicial
+        </div>
+        <div class="ItemFilaC">
+            Origen e
+        </div>
+        <div class="ItemFilaC">
+            10%
+        </div>
+        <div class="ItemFilaC">
+            dd/mm/aaaa
+        </div>
+    </div>
+
+    <!-- Corresponde a una fila de la tabla -->
+    <div class="ContenedorFilasContenido">
+        <div class="ItemFilaC">
+            551d8b
+        </div>
+        <div class="ItemFilaC">
+            {iconos}
+        </div>
+        <div class="ItemFilaC">
+            Nombre (CURP)
+        </div>
+        <div class="ItemFilaC">
+            Solicitud-Ejemplo
+        </div>
+        <div class="ItemFilaC">
+            Departamento Inicial
+        </div>
+        <div class="ItemFilaC">
+            Origen e
+        </div>
+        <div class="ItemFilaC">
+            10%
+        </div>
+        <div class="ItemFilaC">
+            dd/mm/aaaa
+        </div>
+    </div>
+
+
 </div>
+
+<!-- Botones de la tabla -->
+<div class="ContenedorDetallesTabla">
+    <!-- Flecha direccional [Fijo] -->
+    <div class="ElementoIconoPaginaContenedor ElementoIconoPaginaContenedorFlecha">
+        <span>
+            &#60;
+        </span>
+    </div>
+
+    <!-- Numero Pagina [Dinamico]-->
+    <div class="ElementoIconoPaginaContenedor">
+        <span>
+            N
+        </span>
+    </div>
+
+    <!-- Flecha direccional [Fijo] -->
+    <div class="ElementoIconoPaginaContenedor ElementoIconoPaginaContenedorFlecha">
+        <span>
+            &#62;
+        </span>
+    </div>
+</div>
+
+<!-- Información de la tabla -->
